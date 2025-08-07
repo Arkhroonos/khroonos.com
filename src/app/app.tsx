0a0fb61dba1d40e271ac8abcee47492008da5c7b
Khroonos <shoka.khr@gmail.com>
2025-08-07 15:25:53 +0200

    Created Grid Layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
-import 'src/app/app.module.scss';
+import styles from 'src/app/app.module.scss';
 
 import NavbarComponent from './NAVBAR/navbar.component';
 import InfosComponent from './INFOS/infos.component';
@@ -16,18 +16,40 @@ import HobbiesComponent from './HOBBIES/hobbies.component';
 
 export function App() {
   return (
-    <main className="app-container">
-      <NavbarComponent />
-      <InfosComponent />
-      <ApprenticeshipComponent />
-      <LanguagesComponent />
-      <StackComponent />
-      <MotivationComponent />
-      <ProjectsComponent />
-      <SoftwareComponent />
-      <SkillsComponent />
-      <ExperienceComponent />
-      <HobbiesComponent />
+    <main className={styles['app-container']}>
+      <div className={styles.navbar}>
+        <NavbarComponent />
+      </div>
+      <div className={styles.infos}>
+        <InfosComponent />
+      </div>
+      <div className={styles.apprenticeship}>
+        <ApprenticeshipComponent />
+      </div>
+      <div className={styles.languages}>
+        <LanguagesComponent />
+      </div>
+      <div className={styles.stack}>
+        <StackComponent />
+      </div>
+      <div className={styles.motivation}>
+        <MotivationComponent />
+      </div>
+      <div className={styles.projects}>
+        <ProjectsComponent />
+      </div>
+      <div className={styles.software}>
+        <SoftwareComponent />
+      </div>
+      <div className={styles.skills}>
+        <SkillsComponent />
+      </div>
+      <div className={styles.experience}>
+        <ExperienceComponent />
+      </div>
+      <div className={styles.hobbies}>
+        <HobbiesComponent />
+      </div>
     </main>
   );
 }
