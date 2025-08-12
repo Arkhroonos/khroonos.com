d0c50f3bed054240dc1abc956cc7a672b37cb872
Khroonos <shoka.khr@gmail.com>
2025-08-12 18:32:24 +0200

    Added "Contact me" button in navbar.component.tsx

--- src/app/NAVBAR/navbar.component.tsx
+++ src/app/NAVBAR/navbar.component.tsx
@@ -3,7 +3,12 @@ import styles from './navbar.component.module.scss';
 export function NavbarComponent() {
   return (
     <div className={styles['container']}>
+      <div className={styles['content-wrapper']}>
       <h1>NavbarComponent!</h1>
+      <div className={styles['mail-button']}>
+        <a href="mailto: priam.axel@gmail.com">Contact Me !</a>
+      </div>
+      </div>
     </div>
   );
 }
