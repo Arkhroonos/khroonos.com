874beb2ae177cdc2f8a05ea07ca8aab445221991
Khroonos <shoka.khr@gmail.com>
2025-10-01 21:10:17 +0200

    Created Smartphone navbar

--- src/app/navbar/navbar.component.tsx
+++ src/app/navbar/navbar.component.tsx
@@ -1,14 +1,23 @@
 /**
  * React Imports
  */
+import { useState } from 'react';
 
 /**
  * Internal Imports
  */
 
 import styles from './navbar.component.module.scss';
+import ThemeSwitcher from '../THEME/theme-switcher';
 
 export function NavbarComponent() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
+  const handleLinkClick = () => {
+    closeMenu();
+  };
+
   return (
     <section className={styles['navbar']}>
       <div className={styles['container']}>
@@ -42,6 +51,57 @@ export function NavbarComponent() {
               Experiences
             </a>
           </div>
+        </div>
+      </div>
+
+      <div className={styles['menu-wrapper']}>
+        <div className={styles['menu-button']}>
+          <button onClick={toggleMenu}>
+            <img
+              className={styles['icon']}
+              src={'src/assets/medias/icons/icons/home.svg'}
+              alt="menu button"
+            />
+          </button>
+          {isMenuOpen && (
+            <div className={styles['menu-content']}>
+              <div className={styles['menu-item']}>
+                <div className={styles['nav-link']}>
+                  <a href="/" className={styles['link']} onClick={handleLinkClick}>
+                    Home
+                  </a>
+                </div>
+                <div className={styles['nav-link']}>
+                  <a href="/about" className={styles['link']} onClick={handleLinkClick}>
+                    About Me
+                  </a>
+                </div>
+                <div className={styles['nav-link']}>
+                  <a href="/projects" className={styles['link']} onClick={handleLinkClick}>
+                    Projects
+                  </a>
+                </div>
+                <div className={styles['nav-link']}>
+                  <a href="/#stack" className={styles['link']} onClick={handleLinkClick}>
+                    Stack
+                  </a>
+                </div>
+                <div className={styles['nav-link']}>
+                  <a href="/#experiences" className={styles['link']} onClick={handleLinkClick}>
+                    Experiences
+                  </a>
+                </div>
+                <div className={styles['nav-link']}>
+                  <a href="/#contact" className={styles['link']} onClick={handleLinkClick}>
+                    Contact
+                  </a>
+                </div>
+                <div className={styles['theme-switcher']}>
+                  <ThemeSwitcher />
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </section>
